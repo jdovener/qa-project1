523db8ab24228524af91816a23d926323d8af32b
jdovener <jdovener16@gmail.com>
2022-06-01 14:03:40 +0000

    JD: added GCP database, also minor bug fix on payment page

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
