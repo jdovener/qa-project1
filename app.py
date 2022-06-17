1334179ee500853a1aaa971bed1ae8fb0e28ddf0
jdovener <jdovener16@gmail.com>
2022-06-17 10:18:28 +0000

    JD: jenkins database fix attempt

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ from application import db, app
 # run app code
 
 if __name__ == "__main__":
-    db.create_all()
+    # db.drop_all()
+    # db.create_all()
     app.run(debug=True, host='0.0.0.0', port=5000)
